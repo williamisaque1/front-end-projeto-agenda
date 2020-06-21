84c2c22d2ab4e71bf3cd2c9b9634d9fb340bddfc
william isaque <williamisaque18@gmail.com>
2020-06-21 01:47:07 -0300

    quase finalizando a parte visual

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,9 +1,11 @@
-import React from 'react';
+import React from "react"; //acessar sem ser por defalut abre chaves {}
 import {BrowserRouter,Route} from 'react-router-dom';    //quando e arquivo que comeca @types posso usa flag -D vai para desenvolvimento n para producao
-const Routes : React.FC = () =>{
+import Home from "./pages/index"
+const Routes : React.FC = () =>{  //para ver oque tem dentro router dom abaixa o tipagem rect router dom
 return (
     <BrowserRouter>
-    <Route component = {} path = "/" />
+    <Route component = {Home} path = "/" exact />
     </BrowserRouter>
 )
 }
+export default Routes;

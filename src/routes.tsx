7704928fb3c9ac07278808e1df2a600396ae5b94
william isaque <williamisaque18@gmail.com>
2020-06-20 23:50:58 -0300

    inicio do front end

---
+++ src/routes.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import {BrowserRouter,Route} from 'react-router-dom';    //quando e arquivo que comeca @types posso usa flag -D vai para desenvolvimento n para producao
+const Routes : React.FC = () =>{
+return (
+    <BrowserRouter>
+    <Route component = {} path = "/" />
+    </BrowserRouter>
+)
+}
